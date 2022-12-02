db559f224d1cf52f81f4b58880069840b06c0bf1
Maggie Ward <mlward639@gmail.com>
2022-12-02 15:32:13 -0500

    homepage added, still needs final styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <h2>Home page content</h2>;
-};
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
-export default Home;
+function Homepage() {
+  //background image for homepage
+  const myStyle = {
+    backgroundImage: `url(${
+      process.env.PUBLIC_URL + "/images/backgroundImage.jpeg"
+    })`,
+    height: "100vh",
+    //prob dont need, clean up later if not used
+    //marginTop: "-6.5%", //update this size to whatever size the header is set to and figure out how to make mobile responsive;  or maybe change the margin of the header and put this top margin at 0 --fixed i think
+    // fontSize: "50px",
+    marginTop: "-60px",
+    zIndex: "-1",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
+  return (
+    <div className="homepageDiv" style={myStyle}>
+      <div className="logoDiv">
+        <img src={logo} alt="Beyond Eagles' Wings Logo" className="logoImg" />
+      </div>
+      <div className="homepageContent">
+        <button className="learnMoreBtn">
+          <Link to="/info" style={{ textDecoration: "none" }}>
+            Learn More...
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
