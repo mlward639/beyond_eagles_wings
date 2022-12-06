384e781ff1411b613acf75fd9fb38fd7aca007ce
Maggie Ward <mlward639@gmail.com>
2022-12-06 15:03:05 -0500

    company info styling added

--- client/src/pages/CompanyInfo.js
+++ client/src/pages/CompanyInfo.js
@@ -5,7 +5,7 @@ function Info() {
     <div className="row infoDiv">
       <div className="compInfo col-7 col">
         <h1 className="compInfoTitle">Transforming The End of Life Journey</h1>
-        <p>
+        <p className="compInfoContent">
           As an end of life doula, I am honored to assist individuals in
           creating the path they envision for their end of life experience. As
           you review your life, I can guide you in exploring its meaning,
@@ -15,7 +15,7 @@ function Info() {
           support and guidance to your loved ones throughout this time and
           during the weeks following.
         </p>
-        <p>
+        <p className="compInfoContent">
           Our society has altered what was once a natural process into a medical
           one of painful procedures, loud monitoring devices and sterile
           hospital rooms devoid of personal surroundings. As your end of life
@@ -27,25 +27,27 @@ function Info() {
           <div className="missionDiv">
             <h3>My Mission</h3>
             <p>
-              To support and guide the individual and their loved ones during
-              the end of life process to transform their experience into one of
-              deep meaning and peaceful transition. To provide assistance in
+              Support and guide the individual and their loved ones during the
+              end of life process to transform their experience into one of deep
+              meaning and peaceful transition. To provide assistance in
               reprocessing and early grief support for loved ones.
             </p>
           </div>
           <div className="visionDiv">
             <h3>My Vision</h3>
             <p>
-              To transform the natural process of dying for all individuals into
-              a celebration of life and a gift of legacy to take root and grow
-              in those left behind.
+              Transform the natural process of dying for all individuals into a
+              celebration of life and a gift of legacy to take root and grow in
+              those left behind.
             </p>
           </div>
         </div>
       </div>
-      <div className="compInfoQuote  col-3 col">
-        "Return control of your final journey back to you and your loved ones to
-        make it the beautiful, memorable transition it was meant to be."
+      <div className="compInfoQuoteDiv  col-3 col">
+        <p className="compInfoQuote">
+          "Return control of your final journey back to you and your loved ones
+          to make it the beautiful, memorable transition it was meant to be."
+        </p>
       </div>
     </div>
   );
