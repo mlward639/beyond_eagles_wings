4e98797f9792c5340e4fdcd5b03d7cc7cc908513
Maggie Ward <mlward639@gmail.com>
2022-12-07 10:11:24 -0500

    vigil planning styling, still footer issue

--- client/src/components/Services/VigilPlanning.js
+++ client/src/components/Services/VigilPlanning.js
@@ -1,23 +1,33 @@
 import React from "react";
+import img from "../../assets/images/bed2.jpg";
 
 function VigilPlanning() {
   return (
     <div className="serviceCompDivs">
-      <h1>Vigil Planning</h1>
-      <p>
-        Create the environment and atmosphere in which you wish to spend your
-        final days.
-      </p>
-      <p>Vigil Plans can include:</p>
-      <ul className="VigilPlanList">
-        <li>How to address all senses</li>
+      <div className="planContainer">
+        <div className="planInfo">
+          <h1>Vigil Planning</h1>
+          <p>
+            Create the environment and atmosphere in which you wish to spend
+            your final days.
+          </p>
+          <p>Vigil Plans can include:</p>
+          <ul className="VigilPlanList">
+            <li>How to address all senses</li>
 
-        <li>Interactions with those around you</li>
+            <li>Interactions with those around you</li>
 
-        <li>Symptom management</li>
+            <li>Symptom management</li>
 
-        <li>Meaningful rituals you may envision before or following death</li>
-      </ul>
+            <li>
+              Meaningful rituals you may envision before or following death
+            </li>
+          </ul>
+        </div>
+        <div className="planImg">
+          <img src={img} alt="serene picture of candles, bed" />
+        </div>
+      </div>
     </div>
   );
 }
