b460d8daa11a09ea611422fad27691ee3d29331c
Maggie Ward <mlward639@gmail.com>
2022-12-07 17:34:12 -0500

    addl services styling added, still fix footer

--- client/src/components/Services/AddlServices.js
+++ client/src/components/Services/AddlServices.js
@@ -1,15 +1,25 @@
 import React from "react";
+import img from "../../assets/images/hands.jpg";
 
 function AddlServices() {
   return (
-    <div className="serviceCompDivs addlServicesDiv">
-      <h1>Additional Services</h1>
-      <ul>
-        <li>Respite care</li>
-        <li>Advanced directive planning</li>
-        <li>Funeral planning</li>
-        <li>Continued work on life review, vigil planning, legacy project</li>
-      </ul>
+    <div className="serviceCompDivs">
+      <div className="addlServicesDiv">
+        <div className="addlImg">
+          <img src={img} />
+        </div>
+        <div className="addlInfoDiv">
+          <h1>Additional Services</h1>
+          <ul>
+            <li>Respite care</li>
+            <li>Advanced directive planning</li>
+            <li>Funeral planning</li>
+            <li>
+              Continued work on life review, vigil planning, legacy project
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
