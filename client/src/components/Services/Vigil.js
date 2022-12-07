0e3545c30d309b72411e03697522260b2cec48bd
Maggie Ward <mlward639@gmail.com>
2022-12-07 17:05:47 -0500

    vigil styling added, needs high res pic hands, same footer style issue

--- client/src/components/Services/Vigil.js
+++ client/src/components/Services/Vigil.js
@@ -1,27 +1,36 @@
 import React from "react";
+import img from "../../assets/images/hands2.png";
 
 function Vigil() {
   return (
     <div className="serviceCompDivs">
-      <h1>The Vigil</h1>
-      <p>
-        Rest peacefully in your final days, in the beautiful atmosphere you
-        envisioned.
-      </p>
-      <p>Services I can provide during this time:</p>
-      <ul className="vigilList">
-        <li>
-          Assistance and guidance given to carry out your wishes throughout and
-          following the dying process
-        </li>
-        <li>Emotional support and guidance provided for loved ones</li>
-        <li>Respite care as needed</li>
-        <li>
-          Education provided to explain stages of dying 3-6 weeks following your
-          death, I will meet with loved ones for reprocessing and early grief
-          support unlimited text, phone and email support
-        </li>
-      </ul>
+      <div className="planContainer">
+        <div className="planInfo vigilInfo">
+          <h1>The Vigil</h1>
+          <p>
+            Rest peacefully in your final days, in the beautiful atmosphere you
+            envisioned.
+          </p>
+          <p>Services I can provide during this time:</p>
+          <ul className="vigilList VigilPlanList">
+            <li>
+              Assistance and guidance given to carry out your wishes throughout
+              and following the dying process
+            </li>
+            <li>Emotional support and guidance provided for loved ones</li>
+            <li>Respite care as needed</li>
+            <li>Education provided to explain stages of dying</li>
+            <li>
+              3-6 weeks following your death, I will meet with loved ones for
+              reprocessing and early grief support as well as unlimited text,
+              phone and email support
+            </li>
+          </ul>
+        </div>
+        <div className="vigilImg">
+          <img src={img} alt="pictures of 2 hands shaking" />
+        </div>
+      </div>
     </div>
   );
 }
